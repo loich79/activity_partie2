73eaf7e39a2d2e4d9f1cda85e15d3ca18d740d36
loich79 <loic.hay79@gmail.com>
2017-04-05 23:30:30 +0200

    Ajout de code HTML pour le formulaire du fichier test_regex.php

--- test_regex.php
+++ test_regex.php
@@ -1,2 +1,22 @@
-
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>test de regex</title>
+    </head>
+    <body>
+        <form method="post" action="test.php">
+            <p >
+                <label for="regex">Expression régulière : </label>
+                <input type="text" name="regex" id="regex"  />
+            </p>
+            <p>
+                <label for="texte">Texte à tester : </label>
+                <input type="text" name="texte" id="texte"  />
+            </p>
+            <p>
+                <input type="submit" value="Valider">
+            </p>
+        </form>     
+    </body>
+</html>
 
