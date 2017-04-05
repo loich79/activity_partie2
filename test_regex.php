ea166babe601de475849db5448bd9d2b47ca9d27
loich79 <loic.hay79@gmail.com>
2017-04-05 23:43:24 +0200

    ajout du code php au fichier test_regex.php

--- test_regex.php
+++ test_regex.php
@@ -4,19 +4,49 @@
         <title>test de regex</title>
     </head>
     <body>
-        <form method="post" action="test.php">
+        <?php
+        //on affecte une valeur à regex avec un test pour la première excution de la page
+        if(isset($_POST['regex'])) {
+            $regex = $_POST['regex'];
+        } else {
+            $regex = '';
+        }
+        //on affecte une valeur à texte avec un test pour la première excution de la page
+        if(isset($_POST['texte'])) {
+            $texte = $_POST['texte'];
+        } else {
+            $texte = '';
+        }
+        ?>
+        <!-- affiche le formulaire-->
+        <form method="post" action="test_regex.php">
             <p >
                 <label for="regex">Expression régulière : </label>
-                <input type="text" name="regex" id="regex"  />
+                <input type="text" name="regex" id="regex" value="<?= $regex ?>" />
             </p>
             <p>
                 <label for="texte">Texte à tester : </label>
-                <input type="text" name="texte" id="texte"  />
+                <input type="text" name="texte" id="texte" value="<?= $texte ?>" />
             </p>
             <p>
                 <input type="submit" value="Valider">
             </p>
-        </form>     
+        </form>
+         <!-- test du texte avec l'expression réguliere -->
+        <?php 
+        // vérification de la présence d'un expression régulière à tester
+        if($regex != '') {
+            //test du texte avec l'expression régulière
+            if (preg_match($regex, $texte))
+            {
+                echo 'Résultat : vrai';
+            }
+            else
+            {
+                echo 'Résultat : faux';
+            }
+        } 
+        ?>
     </body>
 </html>
 
